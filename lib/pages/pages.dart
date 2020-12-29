8ebed043c80d238b4eecb120c0f7f816fa2a82df
Admor Aguilar <admor.aguilar@gmail.com>
2020-12-30 02:28:55 +0800

    * SearchPage: Initial WIP

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -4,6 +4,7 @@ export 'counter/counter_screen.dart';
 export 'counter/counter_viewmodel.dart';
 export 'creator/creator_page.dart';
 export 'explore/explore_page.dart';
+export 'search/search_page.dart';
 export 'error/error_page.dart';
 export 'support/support_list_viewmodel.dart';
 export 'support/support_page.dart';
--- lib/root_setup.dart
+++ lib/root_setup.dart
@@ -9,16 +9,8 @@ class RootSetup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FlowApp(
-      initialContext: ToExplorePage(),
-      map: FlowMap([
-        ToHomeRouteResponse(),
-        ToExplorePageResponse(),
-        ToCounterPageResponse(),
-        ToSupportPageResponse(),
-        ToErrorPageResponse(),
-        ToCreatorPageResponse(),
-      ]),
+    return MaterialApp(
+      home: SearchPage(),
       title: 'holohole',
       theme: ThemeData(
         brightness: Brightness.dark,
@@ -37,5 +29,34 @@ class RootSetup extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
     );
+
+    // return FlowApp(
+    //   initialContext: ToExplorePage(),
+    //   map: FlowMap([
+    //     ToHomeRouteResponse(),
+    //     ToExplorePageResponse(),
+    //     ToCounterPageResponse(),
+    //     ToSupportPageResponse(),
+    //     ToErrorPageResponse(),
+    //     ToCreatorPageResponse(),
+    //   ]),
+    //   title: 'holohole',
+    //   theme: ThemeData(
+    //     brightness: Brightness.dark,
+    //     primaryColor: Colors.lightBlue[700],
+    //     accentColor: Colors.lightBlue[700],
+    //     backgroundColor: Colors.black,
+    //     visualDensity: VisualDensity.adaptivePlatformDensity,
+    //     textTheme: TextTheme(
+    //       headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+    //       headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+    //     ),
+    //     scaffoldBackgroundColor: Colors.black,
+    //     bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    //       backgroundColor: Colors.black,
+    //     ),
+    //   ),
+    //   debugShowCheckedModeBanner: false,
+    // );
   }
 }
