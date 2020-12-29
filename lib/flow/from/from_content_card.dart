42449d5b2f31c81646eeebc76f1e1f60350f9db3
Admor Aguilar <admor.aguilar@gmail.com>
2020-12-29 20:53:51 +0800

    * CreatorPage: Test integration with FlowMap with loader before showing

--- lib/flow/from/from_content_card.dart
+++ lib/flow/from/from_content_card.dart
@@ -11,17 +11,16 @@ class FromContentCard extends FlowContext {
 class ToCreatorPageResponse extends FlowResponse<FromContentCard> {
   bool get canRespond => true;
 
-  Future<void> respond() async {
-    Creator creator =
-        await CreatorFactory.fromIdAsync(context.contentDTO.content.creator.id);
-
+  void respond() {
     pages.add(FlowPage(
       key: UniqueKey(),
       name: '/creator',
       child: CreatorPage(
         key: GlobalKey<NavigatorState>(),
-        creator: creator,
-        contentFeedTabs: ContentFeedTabFactory.creator([creator]),
+        creatorBuilder:
+            CreatorFactory.fromIdAsync(context.contentDTO.content.creator.id),
+        contentFeedTabBuilder: (Creator creator) =>
+            ContentFeedTabFactory.creator([creator]),
         onBottomNavigationBarItemTap: (int index) =>
             navigate(ToHomeRoute(index)),
         bottomNavigationBarItems: BottomNavigationFactory.main(),
--- lib/pages/creator/creator_page.dart
+++ lib/pages/creator/creator_page.dart
@@ -3,44 +3,81 @@ import 'package:flutter/rendering.dart';
 import 'package:midnight_flutter/midnight_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:virtualhole_api_client_dart/virtualhole_api_client_dart.dart';
+import 'package:virtualhole_flutter/flow/flow.dart';
 import '../../ui/ui.dart';
 
 class CreatorPage extends StatelessWidget {
   const CreatorPage({
     Key key,
-    @required this.creator,
-    @required this.contentFeedTabs,
+    this.creator,
+    this.creatorBuilder,
+    this.contentFeedTabs,
+    this.contentFeedTabBuilder,
     @required this.onBottomNavigationBarItemTap,
     @required this.bottomNavigationBarItems,
-  })  : assert(creator != null),
-        assert(contentFeedTabs != null),
-        assert(onBottomNavigationBarItemTap != null),
+  })  : assert(onBottomNavigationBarItemTap != null),
         assert(bottomNavigationBarItems != null),
         super(key: key);
 
   final Creator creator;
+  final Future<Creator> creatorBuilder;
+  final List<ContentFeedTab> Function(Creator) contentFeedTabBuilder;
   final List<ContentFeedTab> contentFeedTabs;
   final Function(int index) onBottomNavigationBarItemTap;
   final List<BottomNavigationBarItem> bottomNavigationBarItems;
 
   @override
   Widget build(BuildContext context) {
-    return FlowScaffold(
-      body: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          _CreatorAvatarHighlight(creator: creator),
-          _CreatorSocialLinks(creator: creator),
-          for (ContentFeedTab tab in contentFeedTabs) ...[
-            _CreatorContentFeed(tab: tab),
-            SizedBox(height: 16)
-          ]
-        ],
-        cacheExtent: double.infinity,
-      ),
-      onBottomNavigationBarItemTap: onBottomNavigationBarItemTap,
-      bottomNavigationBarItems: bottomNavigationBarItems,
-    );
+    if (creatorBuilder != null) {
+      return FutureBuilder(
+        future: creatorBuilder,
+        builder: (BuildContext context, AsyncSnapshot<Creator> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return HololiveRotatingImage();
+          }
+
+          if (snapshot.connectionState == ConnectionState.done &&
+              (!snapshot.hasData || snapshot.hasError)) {
+            FlowApp.of(context).map.navigate(ToErrorPage());
+          }
+
+          return FlowScaffold(
+            body: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                _CreatorAvatarHighlight(creator: snapshot.data),
+                _CreatorSocialLinks(creator: snapshot.data),
+                for (ContentFeedTab tab
+                    in contentFeedTabBuilder(snapshot.data)) ...[
+                  _CreatorContentFeed(tab: tab),
+                  SizedBox(height: 16)
+                ]
+              ],
+              cacheExtent: double.infinity,
+            ),
+            onBottomNavigationBarItemTap: onBottomNavigationBarItemTap,
+            bottomNavigationBarItems: bottomNavigationBarItems,
+          );
+        },
+      );
+    } else {
+      return FlowScaffold(
+        body: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            _CreatorAvatarHighlight(creator: creator),
+            _CreatorSocialLinks(creator: creator),
+            for (ContentFeedTab tab in contentFeedTabs) ...[
+              _CreatorContentFeed(tab: tab),
+              SizedBox(height: 16)
+            ]
+          ],
+          cacheExtent: double.infinity,
+        ),
+        onBottomNavigationBarItemTap: onBottomNavigationBarItemTap,
+        bottomNavigationBarItems: bottomNavigationBarItems,
+      );
+    }
   }
 }
 
