1d58e1661f9068b3badc49716b17e2e35d1c09c0
Admor Aguilar <admor.aguilar@gmail.com>
2021-01-15 02:56:32 +0800

    * virtualhole_flutter major folder restructure, mimic how VisualStudio solutions are done

--- packages/midnight_flutter/lib/extensions/scroll_position_extensions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension ScrollPositionExtensions on ScrollPosition {
-  double get normalizedScrollExtent {
-    double range = maxScrollExtent - minScrollExtent;
-    double current = pixels - minScrollExtent;
-    return current / range;
-  }
-}
--- packages/midnight_flutter/lib/flow/flow_route_path.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class FlowRoutePath {
-  FlowRoutePath({
-    @required this.location,
-  }) : assert(location != null);
-
-  final String location;
-}
